f6eb2d6b3b0ee8ed1c4890da81d9f4adf4a1ce13
codery <1250400917@qq.com>
2019-09-28 18:59:09 +0800

    fix NullPointException in PublishController and IndexController,add lombok and  get avatar_url

--- src/main/java/zy/community/controller/AuthorizeController.java
+++ src/main/java/zy/community/controller/AuthorizeController.java
@@ -52,6 +52,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUser.getAvatar_url());
             userMapper.insert(user);
             //登录成功后把token写入到Cookie里
             response.addCookie(new Cookie("token",token));
--- src/main/java/zy/community/controller/IndexController.java
+++ src/main/java/zy/community/controller/IndexController.java
@@ -18,6 +18,7 @@ public class IndexController {
     @GetMapping("/")
         public String index(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
+        if(cookies!=null&&cookies.length!=0)
         for(Cookie cookie:cookies){
             if (cookie.getName().equals("token")){
                 String token = cookie.getValue();
@@ -28,7 +29,6 @@ public class IndexController {
                 break;
             }
         }
-
         return "index";
     }
 
